872fb7832308a8ff918c49c0cdf6bf0c8367ffa8
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-02-07 22:04:50 +0100

    Singleton Use Case

---
+++ dist/singleton/game1.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game1 = void 0;
+const leaderboard_1 = require("./leaderboard");
+class Game1 {
+    constructor() {
+        this.leaderboard = new leaderboard_1.default();
+    }
+    addWinner(position, name) {
+        this.leaderboard.addWinner(position, name);
+    }
+}
+exports.Game1 = Game1;
