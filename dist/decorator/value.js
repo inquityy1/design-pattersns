c0cb0b8228c80758917512e95a4064fa608877ad
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-02-10 17:33:53 +0100

    Decorator Use Case

---
+++ dist/decorator/value.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Value;
+class _Value {
+    constructor(value) {
+        this.value = value;
+    }
+}
+function Value(value) {
+    return new _Value(value);
+}
