cac7127e69bbe11af76238605a56feddc6a699e2
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-02-13 12:38:42 +0100

    Chain Of Resposibility Design Pattern

---
+++ src/chain-of-responsibility/chain-of-responsibility-concept.ts
@@ -0,0 +1,47 @@
+// The Chain of Responsibility Pattern
+
+interface IHandler {
+    handle(payload: number): number;
+}
+
+class Successor1 implements IHandler {
+    handle(payload: number) {
+        console.log(`Successor1 payload = ${payload}`);
+        const test = Math.floor(Math.random() * 2) + 1;
+        if (test === 1) {
+            payload += 1;
+            payload = new Successor1().handle(payload);
+        } else {
+            payload -= 1;
+            payload = new Successor2().handle(payload);
+        }
+        return payload;
+    }
+}
+
+class Successor2 implements IHandler {
+    handle(payload: number) {
+        console.log(`Successor2 payload = ${payload}`);
+        const test = Math.floor(Math.random() * 3) + 1;
+        if (test === 1) {
+            payload = payload * 2;
+            payload = new Successor1().handle(payload);
+        } else if (test === 2) {
+            payload = payload / 2;
+            payload = new Successor2().handle(payload);
+        }
+        return payload;
+    }
+}
+
+class Chain {
+    start(payload: number) {
+        return new Successor1().handle(payload);
+    }
+}
+
+// Client
+const CHAIN = new Chain();
+const PAYLOAD = 1;
+const OUT = CHAIN.start(PAYLOAD);
+console.log(`Finished result = ${OUT}`);
